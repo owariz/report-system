2553908c2f8b7bcc1f08fbe5c082edff76679327
owariz <panitthapwon@gmail.com>
2025-09-16 13:47:35 +0700

    update

--- src/features/student/student.repository.js
+++ src/features/student/student.repository.js
@@ -1,4 +1,5 @@
-const prisma = require('../../core/prisma');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 class StudentRepository {
   async createStudent(studentData) {
